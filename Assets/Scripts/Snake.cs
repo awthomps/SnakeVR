12f5c5bb3862522231488d7e42bee0c419413521
Andrei Thompson <killosquirrel@gmail.com>
2016-03-02 20:39:02 -0800

    completed basic demo of snake movement that needs some more tweeking.

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,18 +4,44 @@ using System.Collections;
 public class Snake : MonoBehaviour {
     public Object prefab;
     private GameObject head;
+    private float speed = 0.01f;
+    private SnakeSegmentBehavior mNextSnakeSegmentBehavior;
+    private Transform stereoCamera;
+    private float spawnTimer = 0.0f;
+    private float spawnTimeDelay = 5.0f;
+    private float moveTimer = 0.0f;
+    private float moveTimeDelay = 1.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         // Create the snake head.
-        print(head);
         head = (GameObject) Instantiate(prefab, transform.position, transform.rotation);
-        SnakeSegmentBehavior snakeSegmentBehavior = head.GetComponent<SnakeSegmentBehavior>();
-        snakeSegmentBehavior.init(0);
-	}
+        mNextSnakeSegmentBehavior = head.GetComponent<SnakeSegmentBehavior>();
+        mNextSnakeSegmentBehavior.init(0, transform.forward);
+
+        //Acquire Camera for directions
+        stereoCamera = transform.FindChild("Head").FindChild("Main Camera");
+    }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	// Update is called once per physics cycle
+	void FixedUpdate () {
+        spawnTimer += Time.deltaTime;
+        moveTimer += Time.deltaTime;
+        Vector3 oldPosition = transform.position;
+        transform.position += (stereoCamera.forward.normalized * speed);
+
+        if (moveTimer >= moveTimeDelay)
+        {
+            if (spawnTimer >= spawnTimeDelay)
+            {
+                spawnTimer = 0.0f;
+                mNextSnakeSegmentBehavior.addNewSnakeSegment(0, transform.position, transform.rotation, stereoCamera.forward);
+            }
+            else
+            {
+                mNextSnakeSegmentBehavior.propogatePosition(transform.position);
+            }
+            moveTimer = 0.0f;
+        }
+    }
 }
--- Assets/Scripts/SnakeSegmentBehavior.cs
+++ Assets/Scripts/SnakeSegmentBehavior.cs
@@ -5,11 +5,12 @@ using System.Collections;
 public class SnakeSegmentBehavior : MonoBehaviour {
     // Members
     private int mStackPosition = 0;
-    private GameObject mNextSegment = null;
-	
+    private Transform mNextSegment = null;
+    private SnakeSegmentBehavior mNextSnakeSegmentBehavior;
+
     // Begin Unity Engine Code
     // Use this for initialization
-	void Start () {
+    void Start () {
 
 	}
 	
@@ -19,34 +20,48 @@ public class SnakeSegmentBehavior : MonoBehaviour {
 	}
     //End Unity Engine Code
 
-    public void init(int stackPosition) {
+    public void init(int stackPosition, Vector3 forward) {
         mStackPosition = stackPosition;
+        transform.forward = forward;
     }
 
     // Add new end to the snake.
-    public void addNewSnakeSegment(int stackPosition, Vector3 position, Quaternion rotation)
+    public void addNewSnakeSegment(int stackPosition, Vector3 position, Quaternion rotation, Vector3 newForward)
     {
         //Increment the position
         stackPosition++;
+        Vector3 oldForward = transform.forward;
+        transform.forward = newForward;
+        Vector3 lastPosition = transform.position;
+        transform.position = position;
         if(isNotTail()) {
             //Not tail, so propogate call.
-            SnakeSegmentBehavior snakeSegmentBehavior = mNextSegment.GetComponent<SnakeSegmentBehavior>();
-            snakeSegmentBehavior.addNewSnakeSegment(stackPosition, position, rotation);
+            mNextSnakeSegmentBehavior = mNextSegment.GetComponent<SnakeSegmentBehavior>();
+            mNextSnakeSegmentBehavior.addNewSnakeSegment(stackPosition, lastPosition, rotation, oldForward);
         }
         else {
             //Is tail so create a new tail.
-            mNextSegment = (GameObject) Instantiate((Object) transform.parent.gameObject, position, rotation);
-            SnakeSegmentBehavior snakeSegmentBehavior = mNextSegment.GetComponent<SnakeSegmentBehavior>();
-            snakeSegmentBehavior.init(stackPosition);
+            mNextSegment = (Transform) Instantiate(transform, lastPosition, rotation);
+            mNextSnakeSegmentBehavior = mNextSegment.GetComponent<SnakeSegmentBehavior>();
+            mNextSnakeSegmentBehavior.init(stackPosition, oldForward);
         }
     }
 
-    // Propogate the position down the snake to the tail.
+    //Propogate the position down the snake to the tail.
     public void propogatePosition(Vector3 newPosition)
     {
-        Vector3 nextPosition = transform.position;
+        Vector3 lastPosition = transform.position;
         transform.position = newPosition;
+        if(isNotTail()) {
+            mNextSnakeSegmentBehavior.propogatePosition(lastPosition);
+        }
         
+    }
+
+    public void move(Vector3 direction, float speed)
+    {
+        transform.forward = direction;
+        transform.position += direction * speed;
     }
 
     public bool isNotTail() {
