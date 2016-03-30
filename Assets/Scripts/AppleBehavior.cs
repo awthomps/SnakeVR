2f733c31c3e9ea9869fc929eb84a951c946211ed
Andrei Thompson <killosquirrel@gmail.com>
2016-03-29 19:37:06 -0700

    -Add in apple ocillation

--- Assets/Scripts/AppleBehavior.cs
+++ Assets/Scripts/AppleBehavior.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class AppleBehavior : MonoBehaviour {
     public static string tagName = "AppleTag";
     public static float radius = 1.0f;
+    public float speed = 1.0f;
+    public float ocillatation = 0.125f;
+    private float time = 0.0f;
+    private float startY;
 	// Use this for initialization
 	void Start () {
         gameObject.tag = tagName;
+        startY = transform.position.y;
+        time = 1 - (2 * Random.value);
     }
 	
 	// Update is called once per frame
-	void Update () {
-	    
+	void Update ()
+    {
+        
 	}
 
-    //
     void FixedUpdate()
     {
-
+        time += Time.deltaTime;
+        float height = (ocillatation * Mathf.Sin(speed * time)) + startY;
+        Vector3 pos = new Vector3(transform.position.x, height, transform.position.z);
+        transform.position = pos;
     }
 }
